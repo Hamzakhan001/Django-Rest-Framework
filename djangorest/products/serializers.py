1764d740b52565f8480d5d124f1e6133124d41a1
HamzaKhan001 <spaceus9@gmail.com>
2023-04-30 22:21:04 +0500

    serializers exceptions

--- djangorest/products/serializers.py
+++ djangorest/products/serializers.py
@@ -3,7 +3,7 @@ from .models import Product
 
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class PrimaryProductSerializer(serializers.ModelSerializer):
     my_discount=serializers.SerializerMethodField(read_only=True)
     class Meta:
         model=Product
@@ -12,3 +12,27 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_my_discount(self,obj):
         # obj is the current instance it has all the properties of current run
         return obj.get_discount(); 
+    
+class SecondaryProductSerializer(serializers.ModelSerializer):
+    my_discount=serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=Product
+        fields=['title','content','price','sale_price','my_discount']
+        
+    def get_my_discount(self,obj):
+        # obj is the current instance it has all the properties of current run
+        # try:
+        #     return obj.get_discount(); 
+        # except:
+        #     return None
+        
+        #when modal is not making instance and direct serializer is used
+        
+        if not hasattr(obj,'id'):
+            return None;
+        if not isinstance(obj,Product):
+            return None
+        return obj.get_discount();
+            
+        
+        
--- djangorest/products/views.py
+++ djangorest/products/views.py
@@ -4,16 +4,20 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.models import Product
-from products.serializers import ProductSerializer
+from products.serializers import PrimaryProductSerializer
 
 
 @api_view(["GET","POST"])
 def api_home(request,*args,**kwargs):
     model_data=Product.objects.all()
     data={}
-     
-    if model_data:
-        # data=model_to_dict(model_data,fields=['id','title','price','sale_price','d'])
-        data=ProductSerializer(model_data)
-    return Response(data)
- 
+    
+    serializer=PrimaryProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+    # if model_data:
+    #     # data=model_to_dict(model_data,fields=['id','title','price','sale_price','d'])
+    #     data=ProductSerializer(model_data)
+    # return Response(data) 
+  
